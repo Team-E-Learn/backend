5c43569bb70129ce0034e0f4f8e979a951153544
Carter Vernon <144601471+cv747@users.noreply.github.com>
2025-02-26 15:05:09 +0000

    added fix to allow CORS

--- src/main.py
+++ src/main.py
@@ -39,6 +39,13 @@ swag: SwagManager = SwagManager(
 )
 
 
+@app.after_request
+def after_request(response: Response) -> Response:
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    return response
+
+
 conn: Connection[TupleRow] = psql_connect(projenv.DB_URL)
 print("Database connected")
 initialise_tables(conn)  # create tables if they don't exist
@@ -71,22 +78,57 @@ swag.add_tag(SwagTag("Organisation", "Organisation related endpoints"))
 swag.add_tag(SwagTag("Module", "Module related endpoints"))
 swag.add_tag(SwagTag("User", "User related endpoints"))
 
-register(Subscriptions,"/v1/user/<int:user_id>/subscriptions",
-         "/v1/user/{user_id}/subscriptions", swag, api)
-register(Profile, "/v1/user/<int:user_id>/profile", "/v1/user/{user_id}/profile", swag, api)
-register(User,"/v1/org/<int:org_id>/module/<int:module_id>/user/<int:user_id>",
-    "/v1/org/{org_id}/module/{module_id}/user/{user_id}", swag, api)
+register(
+    Subscriptions,
+    "/v1/user/<int:user_id>/subscriptions",
+    "/v1/user/{user_id}/subscriptions",
+    swag,
+    api,
+)
+register(
+    Profile, "/v1/user/<int:user_id>/profile", "/v1/user/{user_id}/profile", swag, api
+)
+register(
+    User,
+    "/v1/org/<int:org_id>/module/<int:module_id>/user/<int:user_id>",
+    "/v1/org/{org_id}/module/{module_id}/user/{user_id}",
+    swag,
+    api,
+)
 register(CheckEmail, "/v1/auth/email", "/v1/auth/email", swag, api)
 register(CheckUsername, "/v1/auth/username", "/v1/auth/username", swag, api)
 register(Register, "/v1/auth/register", "/v1/auth/register", swag, api)
 register(Login, "/v1/auth/login", "/v1/auth/login", swag, api)
 register(Verify2FA, "/v1/auth/2fa", "/v1/auth/2fa", swag, api)
 register(VerifyEmail, "/v1/auth/verify-email", "/v1/auth/verify-email", swag, api)
-register(Lessons, "/v1/module/<int:module_id>/lessons", "/v1/module/{module_id}/lessons", swag, api)
-register(Lesson,"/v1/module/lesson/<int:lesson_id>","/v1/module/lesson/{lesson_id}", swag, api)
-register(HomeDashboard, "/v1/user/<int:user_id>/dashboard", "/v1/user/{user_id}/dashboard", swag, api)
-register(ModuleDashboard, "/v1/user/<int:user_id>/dashboard/module/<int:module_id>",
-         "/v1/user/{user_id}/dashboard/module/{module_id}", swag, api)
+register(
+    Lessons,
+    "/v1/module/<int:module_id>/lessons",
+    "/v1/module/{module_id}/lessons",
+    swag,
+    api,
+)
+register(
+    Lesson,
+    "/v1/module/lesson/<int:lesson_id>",
+    "/v1/module/lesson/{lesson_id}",
+    swag,
+    api,
+)
+register(
+    HomeDashboard,
+    "/v1/user/<int:user_id>/dashboard",
+    "/v1/user/{user_id}/dashboard",
+    swag,
+    api,
+)
+register(
+    ModuleDashboard,
+    "/v1/user/<int:user_id>/dashboard/module/<int:module_id>",
+    "/v1/user/{user_id}/dashboard/module/{module_id}",
+    swag,
+    api,
+)
 
 
 swag.start_swag()
