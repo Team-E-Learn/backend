b99e66632a6c8a77a716cb0e46c153c6e2917b0f
Carter Vernon <144601471+cv747@users.noreply.github.com>
2025-02-27 17:53:38 +0000

    added proper start for app

--- src/lib/front/front.py
+++ src/lib/front/front.py
@@ -1,5 +1,7 @@
+from collections.abc import Callable
 from flask.app import Flask
 from flask_restful import Api, Resource
+from werkzeug.wrappers import Response
 
 from lib.swagdoc.swagmanager import SwagManager
 from lib.swagdoc.swagtag import SwagTag
@@ -7,15 +9,16 @@ from lib.swagdoc.swagtag import SwagTag
 
 class Front:
 
-    def __init__(self, app: Flask) -> None:
-        self.__app: Flask = app
-        self.__api: Api = Api(app)
+    def __init__(self, app_name: str) -> None:
+        self.__app: Flask = Flask(app_name)
+        self.__api: Api = Api(self.__app)
         self.__swag: SwagManager = SwagManager(
             self.__app,
             "Python TSE backend API",
             "Examples of how to use the projects python API",
             "0.0.0",
         )
+        self.__after_req: Callable[[Response], Response] | None = None
 
     def register(
         self,
@@ -32,5 +35,14 @@ class Front:
     def add_resource(self, resource: type[Resource], route: str) -> None:
         self.__api.add_resource(resource, route)
 
+    def add_after_request(self, fun: Callable[[Response], Response]) -> None:
+        self.__after_req = fun
+
     def start(self) -> None:
         self.__swag.start_swag()
+
+        if self.__after_req is not None:
+            _ = self.__app.after_request(self.__after_req)
+
+    def run(self) -> None:
+        self.__app.run("0.0.0.0")
